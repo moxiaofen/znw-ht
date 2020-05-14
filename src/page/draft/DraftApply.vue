6540be2d329af8793f735ee191a097bd0a4dd3fa
moxiaofen <lingruozi321@126.com>
2020-05-14 18:22:46 +0800

    待调试

--- src/page/draft/DraftApply.vue
+++ src/page/draft/DraftApply.vue
@@ -10,12 +10,14 @@
     />
 
     <div class="content">
-      <van-cell title="选择授信合同" value="" />
+      <van-cell title="选择合同" value="" />
       <van-field name="radio" label="">
         <template #input>
           <van-radio-group class="w100"  v-model="radio" direction="columns" value='radio'>
-            <van-radio  class="credit-item" :name="index" v-for='(item,index) in creditList' :key='index'>
-                <van-cell center :title="item.creditContractNo" :value="item.contractEndDate" :label="item.custName" />             
+            <van-radio  class="credit-item" :name="index" v-for='(item,index) in contractList' :key='index'>             
+                <span class="hide">{{item.contractStatus}}}</span>
+                <span class="hide">{{item.contractNo}}}</span>
+                <van-cell center :title="item.contractCode" :value="formatType(item.contractType)" :label="item.contractName" />             
             </van-radio>
           </van-radio-group>
         </template>
@@ -42,8 +44,9 @@
         enable: true, //立即注册 按钮默认不可用
         
         radio: '',
-        creditList:[],
-        creditContractNo:'',
+        contractList:[],
+        contractNo:'',
+        contractType:'',
       }
     },
     mounted() {
@@ -57,16 +60,20 @@
     methods: {
       //获取新增时的数据
       queryContractList() {
-           const url = this.$api.ROOT + this.$Constants.QUERY_CONTRACT_LIST;
+           const url = this.$api.ROOT + this.$Constants.FIND_CONTRACT_LIST;
+           //const url = this.$api.ROOT + this.$Constants.QUERY_CONTRACT_LIST;
+          
            const data = {
              //"custNo":sessionStorage.getItem('custNo'),
              "custNo":'C000250',
            }
-           this.$http.post(url,{"creditContract":data})
+           this.$http.post(url,data)
+           //this.$http.post(url,{"creditContract":data})
            .then(function (res) { 
+             console.log(res)
                const data = JSON.parse(res.data);
-               this.creditList = data.records 
-               console.log(this.creditList)                               
+               console.log(data)
+               this.contractList = JSON.parse(res.data)
           }).catch(function () {
              this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
           })
@@ -76,18 +83,35 @@
               this.$toast('请先选择合同');
               return
           }  
-          this.creditContractNo = this.creditList[this.radio].creditContractNo;       
-          console.log(this.creditList[this.radio].creditContractNo)
+          this.contractNo = this.contractList[this.radio].contractNo; 
+          this.contractType = this.contractList[this.radio].contractType;      
+          console.log(this.contractList[this.radio].contractNo)
           console.log(this.radio)
          
           this.$router.push({
             path: this.$RM.DraftApplyDetail,
-            //query:{id: this.creditContractNo} 
-            query:{id: 'CON2019090200000412'}        
+            //query:{id: 'CON2019082100000320'} 
+            query:{
+              id: this.contractNo,
+              type:this.contractType
+            }        
           })
       },
       cancel(){
           this.showPop = false    
+      },
+      //格式化合同类型 contract_type：01：定保理   02：池保理   03 ：票据保理
+      formatType(num){
+        switch(num) {
+            case "01":
+                return "定保理" 
+                break;
+            case "02":
+                return "池保理" 
+                break;
+            default:
+                return "票据保理"  
+        } 
       },
 
     }
--- src/page/draft/DraftApplyDetail.vue
+++ src/page/draft/DraftApplyDetail.vue
@@ -11,54 +11,66 @@
 
     <div class="content">
       <!--与顶部保持距离-->
-      <div class='item'>
-        <span class='item-left'>可用金额</span>
-        <span class='item-right'>{{limitAmount}}</span>
+      <div v-if='this.showC'>
+          <div class='item'>
+            <span class='item-left'>可用金额</span>
+            <span class='item-right'>{{limitAmount}}</span>
+          </div>
+          <div class='top'>
+            <span class='inputLable item-input'>融资金额</span>
+            <input class='inputVal' type="text" ref="rApplyAmt" v-model="vApplyAmt" placeholder="请输入融资金额" />
+            <img class='clearCss' v-show="vApplyAmt" @click="clear('vApplyAmt')" src="../../assets/clear.png"/>
+          </div>
       </div>
 
-      <div class='item'>
-        <span class='item-left'>融资金额</span>
-        <span class='item-right'>{{resData.applyAmt}}</span>
-      </div>
+      <div  v-if='!this.showC'>
+          <div class='item'>
+            <span class='item-left'>融资金额</span>
+            <span class='item-right'>{{resData.applyAmt}}</span>
+          </div>
+          <div class='item'>
+            <span class='item-left'>融资期限(天)</span>
+            <span class='item-right'>{{resData.repayTerm}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>融资期限(天)</span>
-        <span class='item-right'>{{resData.repayTerm}}</span>
-      </div>
+          <div class='item'>
+            <span class='item-left'>最长宽限期天数(天)</span>
+            <span class='item-right'>{{resData.maxExtendDays}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>最长宽限期天数(天)</span>
-        <span class='item-right'>{{resData.maxExtendDays}}</span>
-      </div>
+          <div class='item'>
+            <span class='item-left'>保证金比例(%)</span>
+            <span class='item-right'>{{resData.despositAmtRate}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>保证金比例(%)</span>
-        <span class='item-right'>{{resData.despositAmtRate}}</span>
-      </div>
+          <div class='item'>
+            <span class='item-left'>融资比例(%)</span>
+            <span class='item-right'>{{resData.financeScale}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>融资比例(%)</span>
-        <span class='item-right'>{{resData.financeScale}}</span>
-      </div>
+          <div class='item'>
+            <span class='item-left'>保理融资费年费率(%)</span>
+            <span class='item-right'>{{resData.financRate}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>保理融资费年费率(%)</span>
-        <span class='item-right'>{{resData.financRate}}</span>
-      </div>
+          <div class='item'>
+            <span class='item-left'>保理服务费年费率(%)</span>
+            <span class='item-right'>{{resData.feeAmtRate}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>保理服务费年费率(%)</span>
-        <span class='item-right'>{{resData.feeAmtRate}}</span>
-      </div>
+          <div class='item'>
+            <span class='item-left'>宽限期管理费年费率(%)</span>
+            <span class='item-right'>{{resData.extendRate}}</span>
+          </div>
 
-      <div class='item'>
-        <span class='item-left'>宽限期管理费年费率(%)</span>
-        <span class='item-right'>{{resData.extendRate}}</span>
+          <div class='item'>
+            <span class='item-left'>逾期管理费年费率(%)</span>
+            <span class='item-right'>{{resData.pintRate}} </span>
+          </div>
       </div>
-
-      <div class='item'>
-        <span class='item-left'>逾期管理费年费率(%)</span>
-        <span class='item-right'>pintRate</span>
+      
+      <div class="btn-box-fix">
+        <LoginButton name="确定" :isInputNonEmpty="enable" @click.native="comfirm" borderRadius="6px"></LoginButton>
       </div>
 
     </div>
@@ -67,36 +79,58 @@
 </template>
 
 <script>
+  import LoginButton from '@/components/LoginButton.vue'
+
   export default {
     name: "BusinessApplyDetail",
     components: {
+        LoginButton,
     },
     data() {
       return {
         nonet: false, //断网
         enable: true, //立即注册 按钮默认不可用
         resData:{},
+        limitAmount:'',
+        vApplyAmt:'',
+        showC:'',//池保理状态
+        type:'',//保理类型
+        
       }
     },
-    created() {
-        this.FindPoolLimitAmount()
-        this.queryContractList()
+    mounted() {
+        // 池保理，显示可用金额，融资金额填
+        // 定票保理全部显示，融资金额去除
+        //contract_type：01：定保理   02：池保理   03 ：票据保理
+        this.type = this.$route.query.type;
+        if( this.type == '01'|| this.type == '03'){
+            this.showC = false
+            this.queryContractList()
+        }else{
+            this.FindPoolLimitAmount()
+            this.showC = true
+        }
     },
-
     methods: {
+      //清除输入框数据
+      clear (str) {
+        this[str] = ''
+      },
       //app/findPoolLimitAmount 查询池保理池中可用余额(根据合同编号)
       FindPoolLimitAmount() {          
            const url = this.$api.ROOT + this.$Constants.FIND_POOL_LIMIT_AMOUNT;
            const data = {
-             "contractNo":this.$route.query.id,
-             "custNo": sessionStorage.getItem('custNo'),
+             //"contractNo":this.$route.query.id,
+             //"custNo": sessionStorage.getItem('custNo'), 
+             "contractNo": "CON2019062800000079",
+             "custNo": "C000311"
            }
            this.$http.post(url,data)
            .then(function (res) {
-                console.log(res)
-                //this.resData = JSON.parse(res.data);          
+                const resDataAmount = JSON.parse(res.data)
+                this.limitAmount = resDataAmount.creditlimitBal;
           }).catch(function () {
-             this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+                this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
           })
       },   
       //应收账款转让详情信息查询(通过合同编号)
@@ -107,14 +141,30 @@
              "contractNo":this.$route.query.id,
            }
            this.$http.post(url,data)
+           .then(function (res) {              
+                this.resData = JSON.parse(res.data);
+                console.log(this.resData)          
+          }).catch(function () {
+             this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+          })
+      }, 
+      //确定提交  
+      comfirm(){ 
+           const url = this.$api.ROOT + this.$Constants.LOAD_INFO_APPLY;
+           let data 
+           if( this.type == '01'|| this.type == '03'){
+                data.vApplyAmt = this.vApplyAmt
+           }else{
+                data = this.resData
+           }
+           this.$http.post(url,{'resDataAmount' : data })
            .then(function (res) {
                 //console.log(res)
                 this.resData = JSON.parse(res.data);          
           }).catch(function () {
              this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
           })
-      },   
-
+      },
 
     }
 
@@ -146,6 +196,10 @@
     margin-top: 46px;
     position: relative;
   }
- 
+ .item-input{
+    font-size: 0.42667rem;
+    color: #343434;
+    font-weight: bolder;
+ }
   
 </style>
