382ffd2da2987e3061fa51c6859c4d8853693515
moxiaofen <lingruozi321@126.com>
2020-05-14 10:34:36 +0800

    first commit

---
+++ src/page/draft/DraftApply.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <van-nav-bar
+      class="bar"
+      title="提款申请"
+      left-arrow
+      fixed
+      border='false'
+      @click-left="$router.back(-1)"
+    />
+
+    <div class="content">
+      <van-cell title="选择授信合同" value="" />
+      <van-field name="radio" label="">
+        <template #input>
+          <van-radio-group class="w100"  v-model="radio" direction="columns" value='radio'>
+            <van-radio  class="credit-item" :name="index" v-for='(item,index) in creditList' :key='index'>
+                <van-cell center :title="item.creditContractNo" :value="item.contractEndDate" :label="item.custName" />             
+            </van-radio>
+          </van-radio-group>
+        </template>
+      </van-field>
+
+      <div class="btn-box-fix">
+        <LoginButton name="确定" :isInputNonEmpty="enable" @click.native="comfirm" borderRadius="6px"></LoginButton>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  import LoginButton from '@/components/LoginButton.vue'
+  export default {
+    name: "BusinessApply",
+    components: {
+      LoginButton
+    },
+    data() {
+      return {
+        nonet: false, //断网
+        enable: true, //立即注册 按钮默认不可用
+        
+        radio: '',
+        creditList:[],
+        creditContractNo:'',
+      }
+    },
+    mounted() {
+        if(!sessionStorage.getItem('custNo')){
+          this.$toast('请先登录');          
+          return
+        }
+        this.queryContractList()//查询可授信合同数据
+    },
+
+    methods: {
+      //获取新增时的数据
+      queryContractList() {
+           const url = this.$api.ROOT + this.$Constants.QUERY_CONTRACT_LIST;
+           const data = {
+             //"custNo":sessionStorage.getItem('custNo'),
+             "custNo":'C000250',
+           }
+           this.$http.post(url,{"creditContract":data})
+           .then(function (res) { 
+               const data = JSON.parse(res.data);
+               this.creditList = data.records 
+               console.log(this.creditList)                               
+          }).catch(function () {
+             this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+          })
+      },   
+      comfirm(){ 
+          if(this.radio ===''){
+              this.$toast('请先选择合同');
+              return
+          }  
+          this.creditContractNo = this.creditList[this.radio].creditContractNo;       
+          console.log(this.creditList[this.radio].creditContractNo)
+          console.log(this.radio)
+         
+          this.$router.push({
+            path: this.$RM.DraftApplyDetail,
+            //query:{id: this.creditContractNo} 
+            query:{id: 'CON2019090200000412'}        
+          })
+      },
+      cancel(){
+          this.showPop = false    
+      },
+
+    }
+  }
+</script>
+<style >
+  .w100{
+    width: 100%;
+  }
+
+  .credit-item{
+    width: 100%;
+        display: flex;
+  }
+  .credit-item .van-radio__label{
+    flex: 1;
+  }
+</style>
+<style scoped>
+@import "../../css/common";
+  .content {
+    margin-top: 46px;
+    position: relative;
+    padding-bottom: 15px;
+    margin-bottom: 70px;
+  }
+
+</style>
---
+++ src/page/draft/DraftApplyDetail.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <van-nav-bar
+      class="bar"
+      title="提款申请"
+      left-arrow
+      fixed
+      border='false'
+      @click-left="$router.back(-1)"
+    />
+
+    <div class="content">
+      <!--与顶部保持距离-->
+      <div class='item'>
+        <span class='item-left'>可用金额</span>
+        <span class='item-right'>{{limitAmount}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>融资金额</span>
+        <span class='item-right'>{{resData.applyAmt}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>融资期限(天)</span>
+        <span class='item-right'>{{resData.repayTerm}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>最长宽限期天数(天)</span>
+        <span class='item-right'>{{resData.maxExtendDays}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>保证金比例(%)</span>
+        <span class='item-right'>{{resData.despositAmtRate}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>融资比例(%)</span>
+        <span class='item-right'>{{resData.financeScale}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>保理融资费年费率(%)</span>
+        <span class='item-right'>{{resData.financRate}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>保理服务费年费率(%)</span>
+        <span class='item-right'>{{resData.feeAmtRate}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>宽限期管理费年费率(%)</span>
+        <span class='item-right'>{{resData.extendRate}}</span>
+      </div>
+
+      <div class='item'>
+        <span class='item-left'>逾期管理费年费率(%)</span>
+        <span class='item-right'>pintRate</span>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BusinessApplyDetail",
+    components: {
+    },
+    data() {
+      return {
+        nonet: false, //断网
+        enable: true, //立即注册 按钮默认不可用
+        resData:{},
+      }
+    },
+    created() {
+        this.FindPoolLimitAmount()
+        this.queryContractList()
+    },
+
+    methods: {
+      //app/findPoolLimitAmount 查询池保理池中可用余额(根据合同编号)
+      FindPoolLimitAmount() {          
+           const url = this.$api.ROOT + this.$Constants.FIND_POOL_LIMIT_AMOUNT;
+           const data = {
+             "contractNo":this.$route.query.id,
+             "custNo": sessionStorage.getItem('custNo'),
+           }
+           this.$http.post(url,data)
+           .then(function (res) {
+                console.log(res)
+                //this.resData = JSON.parse(res.data);          
+          }).catch(function () {
+             this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+          })
+      },   
+      //应收账款转让详情信息查询(通过合同编号)
+      queryContractList() {
+           //this.contractNo = this.$route.query.id;
+           const url = this.$api.ROOT + this.$Constants.APP_TRANS_APPLY_DETAIL;
+           const data = {
+             "contractNo":this.$route.query.id,
+           }
+           this.$http.post(url,data)
+           .then(function (res) {
+                //console.log(res)
+                this.resData = JSON.parse(res.data);          
+          }).catch(function () {
+             this.$toast(this.$ERRCODE.STATIC_ERRORCDDE.EXCEPTION);
+          })
+      },   
+
+
+    }
+
+  }
+</script>
+<style >
+  .w100{
+    width: 100%;
+  }
+
+  .credit-item{
+    width: 100%;
+        display: flex;
+  }
+  .credit-item .van-radio__label{
+    flex: 1;
+  }
+</style>
+<style scoped>
+@import "../../css/common";
+
+  .item {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+  }
+
+  .content {
+    margin-top: 46px;
+    position: relative;
+  }
+ 
+  
+</style>
